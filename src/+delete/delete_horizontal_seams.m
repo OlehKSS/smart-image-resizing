d7205abda9b552693ad9a2dd1f7350f5a7d05685
Brianna Burton <briannaburton@gmail.com>
2017-12-03 18:25:44 +0100

    finished deleting horz and vert seams

--- src/+delete/delete_horizontal_seams.m
+++ src/+delete/delete_horizontal_seams.m
@@ -1,51 +1,50 @@
 
-%Function that deletes "seams" in image. Pass to the function the original 
-%image, marray and narray, which are the coordinates of the pixels
-%to be deleted, the number of horizontal seams, and the number of vertical
-%seams, respectively. Marray and narray vs should be in the format 
-% 1 x N, where N is the number of pixels to be deleted. The output is the 
-%image with the seams removed.
+%Function that deletes horizontal "seams" in image. Pass to the function
+%the original image, marray and narray, which are the coordinates of the
+%pixels to be deleted, and the number of horizontal seams, respectively.
+%Marray and narray vs should be in the format 1 x N, where N is the number
+%of pixels to be deleted. The output is the image with the horizontal seams
+%removed.
 
-function new_image = delete_seams(image, narray, marray, num_horiz_seams)
+function new_image = delete_horizontal_seams(image, narray, marray, num_horiz_seams)
 
-%Number of total pixels to delete
-num_pixels = length(narray);
+    %Number of total pixels to delete
+    num_pixels = length(narray);
 
-%Size of the original image
-[height, width, ~] = size(image);
-
-%Add one to each value in the image for the deletion condition, in case 
-%zeros already exist.
-image = image + 1.;
-
-%Find the indices of the points to delete and assign 0 to them.
-for i = 1: num_pixels
-    image(marray(i),narray(i)) = 0;
-end
-
-%Flatten the original image into a 1D column vector
-k = 1;
-flattened_image = (1,height*width);
-for i = 1 : height
-    for j = 1 : width
-    flattened_image(k) = image(i,j);
-    k = k + 1;
+    %Size of the original image
+    [height, width, ~] = size(image);
+     if size(image,3)==3
+        depth = 3;
+    else
+        depth = 1;
     end
-end
 
-%Delete all the elements equating to zero
-flattened_image = flattened_image(flattened_image ~= 0);
+    %Add one to each value in the image for the deletion condition, in case 
+    %zeros already exist.
+    addoneimage = image + 1.;
 
-%Subtract one to obtain the original pixel values
-flattened_image = flattened_image - 1.;
+    %Find the indices of the points to delete and assign 0 to them.
+    for z = 1: depth
+        for i = 1: num_pixels
+            addoneimage(marray(i),narray(i),z) = 0;
+        end
+    end
 
-%Reshape the image to the new dimensions
-height = height - num_horiz_seams;
-new_image = zeros(height,width);
-k = 1;
-for i = 1 : height
-    for j = 1 : width
-    new_image(i,j) = flattened_image(k);
-    k = k + 1;
+    %Flatten the original image into a 1D column vector
+    flattened_image = addoneimage(:);
+    
+    %Delete all the elements equating to zero
+    flattened_image = flattened_image(flattened_image ~= 0);
+
+    %Subtract one to obtain the original pixel values
+    flattened_image = flattened_image - 1.;
+
+    %Reshape the image to the new dimensions
+    height = height - num_horiz_seams;
+    new_image = reshape(flattened_image,[height,width,depth]);
+    
+    %Convert new_image to uint8 if it was originally
+    if isfloat(image) == 0
+        new_image = uint8(new_image);
     end
 end
