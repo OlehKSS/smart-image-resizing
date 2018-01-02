a89adfebc8fe0bbc5c813eab4c1b069661c2915e
Karim botros <32384055+Kivo0@users.noreply.github.com>
2018-01-02 21:54:28 +0100

    hog energy fn updated
    + proposal to embed both energy fn.s in one file ,, e1 for grayscale image and hog for RGB image

--- src/+energy_funcs/hog.m
+++ src/+energy_funcs/hog.m
@@ -1,5 +1,7 @@
-function out = hog(im)
-% Convert RGB iamge to grayscale
+function detect = hog(im)
+%from rgb to grayscale ,,,i think it is better to embed both energy fn ,,
+%and choose e1 for grayscale and hog for RGB because e1 has very good
+%results on Grayscale,,, and hog has very good results on RGB image.
 if size(im,3)==3
  im=rgb2gray(im);
 end
@@ -18,19 +20,17 @@ for i=1:cols-2
     Ix(:,i)=(im(:,i)-im(:,i+2));
 end
 
-%gauss= fspecial('gaussian',25000); %% Initialized a gaussian filter with sigma=0.5 * block width.    
-
-angle=atand(Ix./Iy); % Matrix containing the angles of each edge gradient
-angle=imadd(angle,90); %Angles in range (0,180)
 
 magnitude=sqrt(Ix.^2 + Iy.^2);
 
-[pixelCounts, GrayLevels] = imhist(magnitude);
-   maxGrayLevel = max(magnitude(3));
+   GrayLevels = max(magnitude(3));
 
 
-hogy = magnitude/maxGrayLevel;
+hogy = magnitude/GrayLevels;
 %hogy = magnitude/max(histogram(magnitude,'pdf',NM);
-%[featurevec,hogvisualization]=extractHOGFeatures(magnitude);
-out = hogy;
+
+%figure,imshow(uint8(angle));
+    detect = hogy;
+
+
 end
